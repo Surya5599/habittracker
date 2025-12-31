bac5112be3ad44950122822e2c10d84d704416e9
Surya Singh <ssurya5599@gmail.com>
2025-12-30 23:39:58 -0800

    feat: add iOS platform and initial project setup for Capacitor.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.HabiCard.com',
+  appName: 'HabiCard',
+  webDir: 'dist'
+};
+
+export default config;
